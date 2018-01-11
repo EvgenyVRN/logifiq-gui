30691c4d5ad2a250abb2d8e7c5bb34910d0a6144
e-kolosov <kolosove88@gmail.com>
2018-01-11 10:54:46 +0300

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,13 @@
 # 
 SYNC_DIR='dist/'
 
-if [ ! -d $SYNC_DIR ]; then
+# if [ ! -d $SYNC_DIR ]; then
 	ng build -prod
-fi
+# fi
 
 # SITE
 SITE_DIR='/var/www/html/'
 if [ -d $SITE_DIR ]; then
 	rsync -av --delete $SYNC_DIR $SITE_DIR
-	mv $SITE_DIR/index.html $SITE_DIR/expedition 
 fi
 
