19e8bc17e54e2b1a6309e1085fbf25663a0a22eb
e-kolosov <kolosove88@gmail.com>
2018-02-02 15:16:40 +0300

    ---

--- src/app/component/select-address/select-address.component.ts
+++ src/app/component/select-address/select-address.component.ts
@@ -42,7 +42,6 @@ export class SelectAddressComponent implements OnInit {
   }
 
   public selected(value:any):void {
-    console.log('Selected value is: ', value);
     this.address = this.addresses.find(address => address.id === value.id);
     this.addressChange.emit(this.address);
   }
