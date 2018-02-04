02bdaf5a19a292442ce1d0025c45be689f485a3e
dontcrypls36 <Warrior1988>
2018-02-04 10:09:14 +0300

    fix select-contract

--- src/app/component/select-contract/select-contract.component.ts
+++ src/app/component/select-contract/select-contract.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange, SimpleChanges} from '@angular/core';
 import {Contract} from "../../model/contract";
 import {ContractService} from "../../service/contract.service";
 import {Contractor} from "../../model/contractor";
@@ -8,7 +8,7 @@ import {Contractor} from "../../model/contractor";
   templateUrl: './select-contract.component.html',
   styleUrls: ['./select-contract.component.css']
 })
-export class SelectContractComponent implements OnInit {
+export class SelectContractComponent implements OnInit, OnChanges {
   public contracts : Contract[];
   @Input() contract: Contract;
   @Output() contractChange = new EventEmitter();
@@ -28,7 +28,20 @@ export class SelectContractComponent implements OnInit {
     }
   }
 
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const contractor: SimpleChange = changes.contractor;
+    if (contractor !== null){
+      this.getContracts();
+    }
+  }
+
+
   getContracts():void {
+    if (this.contractor == null){
+      return;
+    }
+
     this.contractService.getContractsByContractor(this.contractor.id)
       .subscribe(contract => this.contractAnswer(contract));
   }
@@ -44,7 +57,7 @@ export class SelectContractComponent implements OnInit {
     console.log('Selected value is: ', value);
     this.contractChange.emit(value);
   }
-
+/**/
   public removed(value:any):void {
     console.log('Removed value is: ', value);
   }
